62766557ed9522903ade2d9b6011838635e46366
kazunoriboy <matunorigmnw@gmail.com>
2022-01-16 19:12:18 +0900

    Todoアイテムのコンポーネント作成

---
+++ web/do-well-web/src/components/TaskItem.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+import { Task } from './Types'
+
+type Props = {
+  task: Task
+  handleDone: (task: Task) => void
+  handleDelete: (task: Task) => void
+}
+
+const TaskItem: React.FC<Props> = ({ task, handleDone, handleDelete }) => {
+  return (
+    <li className={task.done ? 'done' : ''}>
+      <input
+        type="checkbox"
+        className="checkbox-input"
+        onClick={ () => handleDone(task) }
+        defaultChecked={task.done}
+        id="checkbox-done"
+      />
+      <label htmlFor="checkbox-done">
+        <span className="checkbox-label">{ task.title }</span>
+      </label>
+      <button
+        onClick={ () => handleDelete(task) }
+        className="btn is-delete"
+      >
+        削除
+      </button>
+    </li>
+  )
+}
+
+export default TaskItem
