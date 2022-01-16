701f4ab7217892f54236619341e9c7200da0b55b
kazunoriboy <matunorigmnw@gmail.com>
2022-01-16 20:05:16 +0900

    サイドバーのコンポーネントを作成

---
+++ web/do-well-web/src/components/Sidebar.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Sidebar = () => {
+  return (
+    <aside className="l-sidebar">
+      <ul className="l-sidebar__list">
+        <li className="l-sidebar__item">ホーム</li>
+        <li className="l-sidebar__item">マイタスク</li>
+      </ul>
+    </aside>
+  )
+}
+
+export default Sidebar;
