5c6f947bd3823b2c4153a01a5b604b7a7495649d
kazunoriboy <matunorigmnw@gmail.com>
2022-01-23 20:33:05 +0900

    フォーマットの実行

--- web/do-well-web/src/components/TaskItem.tsx
+++ web/do-well-web/src/components/TaskItem.tsx
@@ -7,24 +7,22 @@ type Props = {
   handleDelete: (task: Task) => void;
 };
 
-const TaskItem: React.FC<Props> = ({task, handleDone, handleDelete}) => {
-  return (
-    <li className={task.done ? 'done' : ''}>
-      <input
-        type="checkbox"
-        className="checkbox-input"
-        onClick={() => handleDone(task)}
-        defaultChecked={task.done}
-        id="checkbox-done"
-      />
-      <label htmlFor="checkbox-done">
-        <span className="checkbox-label">{task.title}</span>
-      </label>
-      <button onClick={() => handleDelete(task)} className="btn is-delete">
-        削除
-      </button>
-    </li>
-  );
-};
+const TaskItem: React.FC<Props> = ({task, handleDone, handleDelete}) => (
+  <li className={task.done ? 'done' : ''}>
+    <input
+      type="checkbox"
+      className="checkbox-input"
+      onClick={() => handleDone(task)}
+      defaultChecked={task.done}
+      id="checkbox-done"
+    />
+    <label htmlFor="checkbox-done">
+      <span className="checkbox-label">{task.title}</span>
+    </label>
+    <button onClick={() => handleDelete(task)} className="btn is-delete">
+      削除
+    </button>
+  </li>
+);
 
 export default TaskItem;
