4481468a1314a4ac2d06313650a4b714245feea9
kazunoriboy <matunorigmnw@gmail.com>
2022-02-03 09:50:31 +0900

    main部分をpagesに切り出す

---
+++ web/do-well-web/src/layouts/Main.tsx
@@ -0,0 +1,29 @@
+import React, {useState} from 'react';
+import TaskInput from '../layouts/TaskInput';
+import TaskList from '../layouts/TaskList';
+import {Task} from './Types';
+
+const initialState: Task[] = [
+  {
+    id: 2,
+    title: 'タスク2',
+    done: false,
+  },
+  {
+    id: 1,
+    title: 'タスク1',
+    done: false,
+  },
+];
+
+const Main: React.VFC = () => {
+  const [tasks, setTasks] = useState(initialState);
+  return (
+    <main className="l-main">
+      <TaskInput setTasks={setTasks} tasks={tasks} />
+      <TaskList setTasks={setTasks} tasks={tasks} />
+    </main>
+  );
+};
+
+export default Main;
--- web/do-well-web/src/pages/Home.tsx
+++ web/do-well-web/src/pages/Home.tsx
@@ -1,33 +1,12 @@
-import React, {useState} from 'react';
 import '../css/style.scss';
 import Sidebar from '../layouts/Sidebar';
-import TaskInput from '../layouts/TaskInput';
-import TaskList from '../layouts/TaskList';
-import {Task} from '../layouts/Types';
+import Main from '../layouts/Main';
 
-const initialState: Task[] = [
-  {
-    id: 2,
-    title: 'タスク2',
-    done: false,
-  },
-  {
-    id: 1,
-    title: 'タスク1',
-    done: false,
-  },
-];
-
-const Home: React.FC = () => {
-  const [tasks, setTasks] = useState(initialState);
-
+const Home: React.VFC = () => {
   return (
     <div className="f-wrapper">
       <Sidebar />
-      <main className="l-main">
-        <TaskInput setTasks={setTasks} tasks={tasks} />
-        <TaskList setTasks={setTasks} tasks={tasks} />
-      </main>
+      <Main />
     </div>
   );
 };
