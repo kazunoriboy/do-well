241908addb0df2afdd818ac40cb4b625db8738a3
kazunoriboy <matunorigmnw@gmail.com>
2022-02-05 23:06:59 +0900

    検索ボックスのレイアウトを作成

--- web/do-well-web/src/layouts/Main.tsx
+++ web/do-well-web/src/layouts/Main.tsx
@@ -22,7 +22,18 @@ const Main: React.VFC = () => {
     <main className="l-main">
       <div className="l-main__header">
         <div className="c-pageTitle">Home</div>
-        <input type="text" className="c-searchBox" />
+        <form action="" method="get" className="c-searchBox">
+          <svg
+            className="c-searchBoxIcon w-6 h-6"
+            fill="none"
+            stroke="currentColor"
+            viewBox="0 0 24 24"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path>
+          </svg>
+          <input type="text" className="c-searchBoxInput" placeholder="Search" />
+        </form>
         <div className="c-addButton">+</div>
         <div className="c-userMenus"></div>
       </div>
