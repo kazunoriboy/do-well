9fee86c4c75c03f93f97508bea05473b7c8dc114
kazunoriboy <matunorigmnw@gmail.com>
2022-02-05 23:39:35 +0900

    不要なタグを削除

--- web/do-well-web/src/layouts/Main.tsx
+++ web/do-well-web/src/layouts/Main.tsx
@@ -45,7 +45,6 @@ const Main: React.VFC = () => {
             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 6v6m0 0v6m0-6h6m-6 0H6"></path>
           </svg>
         </div>
-        <div className="c-userMenus"></div>
       </div>
       <TaskInput setTasks={setTasks} tasks={tasks} />
       <TaskList setTasks={setTasks} tasks={tasks} />
