edce7913f2652d63649b3246cd6ffcd3d574e6c7
kazunoriboy <matunorigmnw@gmail.com>
2022-02-07 09:58:29 +0900

    区切りのボーダー

--- web/do-well-web/src/layouts/components/DashBoard.tsx
+++ web/do-well-web/src/layouts/components/DashBoard.tsx
@@ -8,8 +8,8 @@ const Dashboard: React.VFC = () => {
           <option value="weekly">My week</option>
           <option value="monthly">My month</option>
         </select>
-        <div className="c-stats__divider"></div>
-        <div className="c-stats__completedTasks">0 tasks completed</div>
+        <div className="c-stats__divider u-ml-1"></div>
+        <div className="c-stats__completedTasks u-ml-1">0 tasks completed</div>
       </div>
     </div>
   )
