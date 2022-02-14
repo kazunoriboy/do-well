24e07d635d94c48186301668a73304dc155c8022
kazunoriboy <matunorigmnw@gmail.com>
2022-02-14 10:38:44 +0900

    formatの実行

--- web/do-well-web/src/layouts/components/TaskBoard.tsx
+++ web/do-well-web/src/layouts/components/TaskBoard.tsx
@@ -1,16 +1,17 @@
 const TaskBoard: React.VFC = () => {
   return (
     <div className="p-taskBoard">
-      <h3 className="p-taskBoard__title">
-        My Priorities
-      </h3>
+      <h3 className="p-taskBoard__title">My Priorities</h3>
       <ul className="p-taskStatusTab u-mt-1">
         <li className="p-taskStatusTab__item">Upcoming</li>
         <li className="p-taskStatusTab__item">Overdue</li>
         <li className="p-taskStatusTab__item">Completed</li>
       </ul>
       <ul className="p-taskList">
-        <li className="c-taskItem">LPIC学習<span className="c-taskItem__label">プライベート</span><span className="c-taskItem__due">Tommorow</span></li>
+        <li className="c-taskItem">
+          LPIC学習<span className="c-taskItem__label">プライベート</span>
+          <span className="c-taskItem__due">Tommorow</span>
+        </li>
       </ul>
     </div>
   );
